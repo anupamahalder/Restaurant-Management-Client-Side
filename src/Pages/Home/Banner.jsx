0314fbd3698cd4e581dff96a9f1a3ddff41a8325
Anupama_Halder <anupamahalder2022@gmail.com>
2024-02-09 13:03:33 +0530

    Added banner in home page

---
+++ src/Pages/Home/Banner.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
+const Banner = () => {
+    const divStyle = 'h-[650px] mx-auto';
+    return (
+        <Carousel>
+            <div className={divStyle}>
+                <img src="https://images.unsplash.com/photo-1544025162-d76694265947?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+            </div>
+            <div className={divStyle}>
+                <img src="https://images.unsplash.com/photo-1628294895933-4360614f39dd?q=80&w=1432&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+            </div>
+            <div className={divStyle}>
+                <img src="https://images.unsplash.com/photo-1659954868279-2d8fe7a0acdb?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+            </div>
+            <div className={divStyle}>
+                <img src="https://images.unsplash.com/photo-1651978595432-905a420b879d?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+            </div>
+            <div className={divStyle}>
+                <img src="https://images.unsplash.com/photo-1622880833523-7cf1c0bd4296?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+            </div>
+            <div className={divStyle}>
+                <img src="https://images.unsplash.com/photo-1565498253128-c06e0488ddee?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+            </div>
+            <div className={divStyle}>
+                <img src="https://images.unsplash.com/photo-1588644525273-f37b60d78512?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+            </div>
+            <div className={divStyle}>
+                <img src="https://images.unsplash.com/photo-1599173687303-7e1eb50f1e60?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+            </div>
+        </Carousel>
+    );
+};
+
+export default Banner;
